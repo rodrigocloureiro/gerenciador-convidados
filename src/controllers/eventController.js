2f76421b52b5d62054d0e8bdd310f2a7fd19d69c
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-03-07 14:15:06 -0300

    metodo get para listar todos os eventos implementado

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,6 +1,14 @@
 import event from '../models/Event.js';
 
 class EventController {
+  static async getAll(req, res) {
+    try {
+      res.status(200).json(await event.find());
+    } catch (err) {
+      res.status(500).json({ message: `Erro ao criar evento: ${err.message}` });
+    }
+  }
+
   static async addEvent(req, res) {
     try {
       const { body } = req;
--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.get('/events', EventController.getAll);
 routes.post('/events', EventController.addEvent);
 
 export default routes;
