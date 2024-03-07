470eaf93b000cdd1e676ae1f589e80643f8eb820
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-03-07 17:45:35 -0300

    metodo para remover um determinado convidado de um evento

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -25,13 +25,13 @@ class EventController {
   static async getVipGuests(req, res) {
     try {
       const { params : { id } } = req;
-      const guests = await event.aggregate([
+      const vips = await event.aggregate([
         { $match: { _id: ObjectId.createFromHexString(id) } }, // busca pelo id (ele cria um ObjectId a partir do id (string))
         { $unwind: "$guests" }, // descontrói o array para gerar um documento para cada elemento
         { $match: { "guests.vip": true } }, // busca todos os elementos descontruídos acima com vip: true
         { $replaceRoot: { newRoot: "$guests" } }, // esta etapa substitui o documento raiz pelo documento 'guests'
       ]);
-      res.status(200).json({ guests });
+      res.status(200).json({ vips });
 
     } catch (err) {
       res.status(500).json({ message: `Erro ao listar vips do evento: ${err.message}` });
@@ -57,6 +57,20 @@ class EventController {
 
       eventById.guests.push(guestById);
       res.status(201).json(await eventById.save());
+
+    } catch (err) {
+      res.status(500).json({ message: `Erro ao convidar para o evento: ${err.message}` });
+    }
+  }
+
+  static async removeGuestEvent(req, res) {
+    try {
+      const { params : { eventId, guestId } } = req;
+      const eventUpdated = await event.updateOne(
+        { _id: ObjectId.createFromHexString(eventId) },
+        { $pull: { guests: { _id: ObjectId.createFromHexString(guestId) } } },
+      );
+      res.status(200).json(eventUpdated);
 
     } catch (err) {
       res.status(500).json({ message: `Erro ao convidar para o evento: ${err.message}` });
--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.delete('/events/:eventId/remove/:guestId', EventController.removeGuestEvent);
 routes.get('/events/:id/vips', EventController.getVipGuests)
 routes.get('/events/:id', EventController.getById);
 routes.get('/events', EventController.getAll);
