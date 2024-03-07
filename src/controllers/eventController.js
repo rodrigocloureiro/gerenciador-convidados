8b3d531b0d251999e1f79964fb9c183f576857af
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2024-03-07 14:21:35 -0300

    metodo get para lista evento filtrado por id implementado

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -5,7 +5,16 @@ class EventController {
     try {
       res.status(200).json(await event.find());
     } catch (err) {
-      res.status(500).json({ message: `Erro ao criar evento: ${err.message}` });
+      res.status(500).json({ message: `Erro ao listar eventos: ${err.message}` });
+    }
+  }
+
+  static async getById(req, res) {
+    try {
+      const { params: { id } } = req;
+      res.status(500).json(await event.findById(id));
+    } catch (err) {
+      res.status(500).json({ message: `Erro ao listar evento: ${err.message}` });
     }
   }
 
